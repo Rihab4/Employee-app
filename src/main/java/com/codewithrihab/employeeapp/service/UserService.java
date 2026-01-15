4a75666c98f85b72fca39750bce8556590618247
rihab <rihab.hassairi@supcom.tn>
2026-01-15 15:18:55 +0100

    customize security config

---
+++ src/main/java/com/codewithrihab/employeeapp/service/UserService.java
@@ -0,0 +1,33 @@
+package com.codewithrihab.employeeapp.service;
+
+import com.codewithrihab.employeeapp.entities.User;
+import com.codewithrihab.employeeapp.repositories.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email)
+            throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found with email: " + email));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPassword())
+                .authorities(user.getRole().name())
+                .build();
+    }
+}
